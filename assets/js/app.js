0dc615236058d62e8046c06eeeffc03a11af840d
Ben <benboorstein@gmail.com>
2022-07-17 09:44:39 -0400

    Minor changes. Project finished for now.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,17 @@
 // VERSION 2:
 
-// Note: I told Robert I think of Version 1 backwards and he said in this case (because, mainly, all functions are stored in memory and the array isn't accessed until later) it's fine to write the code that way. So here, in Version 2, this is what I've done. And there are also other good changes in this version that you need to pay attention to.
-
-document.getElementById('toggle').addEventListener('click', getPlan) // Remember that putting "getPlan" here as the event listener's second argument works because getPlan() is not receiving parameters in this case. If it were, then I'd have to make the event listener's second argument an anonymous function instead (as I did in Version 1, even though I didn't need to there).
-// And remember that the second argument of addEventListener() is ALWAYS (by default, whether you write anything in or not) "event". So here it's fine, because "getPlan" is the event. But if you don't want the second argument to be an event then you need to pass in "e" or "event" as the second argument anyway and then pass in as the third argument what you wanted to pass in as the second argument.
-
-function getPlan() {
-    if (document.getElementById('toggle').checked) { // This also works: "document.getElementById('toggle').checked == true". And this: "document.getElementById('toggle').checked === true". Just FYI: Almost never (or maybe never) in an "if...else" statement's condition should you be using a single equals sign (given what a single equals sign actually does).
-        monthlyPlan(plans)
-    } else {
-        annualPlan(plans)
+const plans = [
+    { // Monthly
+        priceBasic: '19.99',
+        priceProfessional: '24.99',
+        priceMaster: '39.99'
+    },
+    { // Annually
+        priceBasic: '199.99',
+        priceProfessional: '249.99',
+        priceMaster: '399.99'
     }
-}
+]
 
 function monthlyPlan(list) { // Functions should ALWAYS have inputs and expect outputs, and functions should behave exactly the same way every time they're run. And remember that what are passed in as parameters are "the things on which the function will operate". So, in this respect, this version is way better than Version 1. 
     document.getElementById('price-basic').innerText = list[0].priceBasic
@@ -25,18 +25,16 @@ function annualPlan(list) {
     document.getElementById('price-master').innerText = list[1].priceMaster
 }
 
-const plans = [
-    { // Monthly
-        priceBasic: '19.99',
-        priceProfessional: '24.99',
-        priceMaster: '39.99'
-    },
-    { // Annually
-        priceBasic: '199.99',
-        priceProfessional: '249.99',
-        priceMaster: '399.99'
+function getPlan() {
+    if (document.getElementById('toggle').checked) {
+        monthlyPlan(plans)
+    } else {
+        annualPlan(plans)
     }
-]
+}
+
+document.getElementById('toggle').addEventListener('click', getPlan) // Remember that putting "getPlan" here as the event listener's second argument works because getPlan() is not receiving parameters in this case. If it were, then I'd have to make the event listener's second argument an anonymous function instead (as I did in Version 1, even though I didn't need to there).
+
 
 
 /*
